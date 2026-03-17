32c4d0365a9fa9af5316c16bc6f77cc939cec98a
Tu Nombre <dcoletti@gmail.com>
2026-03-17 10:22:10 -0300

    multisearch

--- views/Search/ca_objects_search_subview_html.php
+++ views/Search/ca_objects_search_subview_html.php
@@ -1,32 +1,9 @@
 <?php
 /* ----------------------------------------------------------------------
- * themes/default/views/Search/ca_objects_search_subview_html.php : 
- * ----------------------------------------------------------------------
- * CollectiveAccess
- * Open-source collections management software
- * ----------------------------------------------------------------------
- *
- * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2013-2015 Whirl-i-Gig
- *
- * For more information visit http://www.CollectiveAccess.org
- *
- * This program is free software; you may redistribute it and/or modify it under
- * the terms of the provided license as published by Whirl-i-Gig
- *
- * CollectiveAccess is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- *
- * This source code is free and modifiable under the terms of 
- * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
- * the "license.txt" file for details, or visit the CollectiveAccess web site at
- * http://www.CollectiveAccess.org
- *
- * ----------------------------------------------------------------------
- */
- 
-	$qr_results 		= $this->getVar('result');
+ * Repositorio Digital — Facultad de Filosofía y Letras, UBA
+ * https://filo.uba.ar
+ * ---------------------------------------------------------------------- */
+$qr_results 		= $this->getVar('result');
 	$va_block_info 		= $this->getVar('blockInfo');
 	$vs_block 			= $this->getVar('block');
 	$vn_start		 	= (int)$this->getVar('start');			// offset to seek to before outputting results
