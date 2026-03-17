aea00d714ff3cf7f42979d2ede504479ceb12aa6
Tu Nombre <dcoletti@gmail.com>
2026-03-17 10:26:48 -0300

    multisearch

--- views/Search/multisearch_results_html.php
+++ views/Search/multisearch_results_html.php
@@ -1,43 +1,59 @@
 <?php
 	$va_results = $this->getVar('results');
 	$va_result_count = $va_results['_info_']['totalCount'];
-	if ($va_result_count > 0) {
+	$vs_search = $this->getVar('search');
 ?>
-	<div class="col-sm-1"></div>
-	<div class="col-sm-10">
-		<h4 style='margin-bottom:10px; letter-spacing:1px;float:left;'><?php print _t("Search results for %1", caUcFirstUTF8Safe($this->getVar('searchForDisplay'))); ?></h4>
-	
-		<small class='pull-right' style='margin-top:20px;margin-bottom:10px;'>
+<div class="col-sm-1"></div>
+<div class="col-sm-10">
+
+	<!-- Formulario de búsqueda siempre visible -->
+	<div style="margin: 20px 0 30px 0;">
+		<form role="search" action="<?php print caNavUrl($this->request, '', 'MultiSearch', 'Index'); ?>" method="get">
+			<div class="filo-search-row" style="max-width:600px;">
+				<input type="text" name="search" value="<?php print htmlspecialchars($vs_search); ?>"
+					placeholder="<?php print _t('Buscá documentos, fotografías, fondos...'); ?>"
+					style="flex:1; border:none; padding:12px 18px; font-size:15px; outline:none;" />
+				<button type="submit"
+					style="background:#1a2035; border:none; padding:0 20px; color:#fff; font-size:16px; cursor:pointer;">
+					<span class="glyphicon glyphicon-search"></span>
+				</button>
+			</div>
+		</form>
+	</div>
+
 <?php
-		$i = 0;
-		foreach($this->getVar('blockNames') as $vs_block) {
-			if ($va_results[$vs_block]['count'] == 0) { continue; }
-			$i++;
-			if($i > 1){
-				print " | ";
+	if ($vs_search) {
+		if ($va_result_count > 0) {
+?>
+		<h4 style='margin-bottom:10px; letter-spacing:1px; float:left;'><?php print _t("Resultados para %1", caUcFirstUTF8Safe($vs_search)); ?></h4>
+		<small class='pull-right' style='margin-top:20px; margin-bottom:10px;'>
+<?php
+			$i = 0;
+			foreach($this->getVar('blockNames') as $vs_block) {
+				if ($va_results[$vs_block]['count'] == 0) { continue; }
+				$i++;
+				if($i > 1){ print " | "; }
+				print "<a href='#{$vs_block}'>".$va_results[$vs_block]['displayName']." (".$va_results[$vs_block]['count'].")</a>";
 			}
-			print "<a href='#{$vs_block}'>".$va_results[$vs_block]['displayName']." (".$va_results[$vs_block]['count'].")</a>";
-		}
 ?>
 		</small>
 <?php
-		// 
-		// Print out block content (results for each type of search)
-		//
-		foreach($this->getVar('blockNames') as $vs_block) {
+			foreach($this->getVar('blockNames') as $vs_block) {
 ?>
 			<a name='<?php print $vs_block; ?>'></a>
 			<div id="<?php print $vs_block; ?>Block" class='resultBlock'>
 				<?php print $va_results[$vs_block]['html']; ?>
 			</div>
 <?php
-		} 
-	} else {
-		print "<H3>"._t("Your search for %1 returned no results", caUcFirstUTF8Safe($this->getVar('search')))."</H3>";
+			}
+		} else {
+			print "<p style='color:#666; margin-top:10px;'>"._t("No se encontraron resultados para %1", "<strong>".caUcFirstUTF8Safe($vs_search)."</strong>")."</p>";
+		}
 	}
 ?>
-	</div>
-	<div class="col-sm-1"></div>
+
+</div>
+<div class="col-sm-1"></div>
 <?php
 	TooltipManager::add('#Block', _t('Tipo de registro'));
 ?>
